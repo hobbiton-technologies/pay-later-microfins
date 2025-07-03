7463fe83f30d8d90749c2b4d165b7243ea10108b
SamWaku <samuelwaku1st@gmail.com>
2025-07-03 12:48:07 +0200

    chore: branches table

--- src/modules/business/Branches.tsx
+++ src/modules/business/Branches.tsx
@@ -1,5 +1,19 @@
+import AnimatedHeader from "../components/AnimatedHeader";
+import ProductStats from "../summary/components/ProductStats";
+
 const Branches = () => {
-  return <div>Branches</div>;
+  return (
+    <div>
+      <div>
+        <AnimatedHeader title="Branches" />
+        <p>Interact with your branches</p>
+      </div>
+      <div>
+        <ProductStats />
+        <div>Branches Table</div>
+      </div>
+    </div>
+  );
 };
 
 export default Branches;
