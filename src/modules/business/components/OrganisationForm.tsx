0de1c7cfec9395c0c9f8af042e1cd5a0b1f04845
SamWaku <samuelwaku1st@gmail.com>
2025-07-03 17:46:18 +0200

    chore: laid out form

--- src/modules/business/components/OrganisationForm.tsx
+++ src/modules/business/components/OrganisationForm.tsx
@@ -1,4 +1,4 @@
-import { Form } from "antd";
+import { Button, Form, Input } from "antd";
 import { useState } from "react";
 
 export const OrganisationForm = () => {
@@ -15,7 +15,40 @@ export const OrganisationForm = () => {
         className=" grid grid-cols-1 gap-8 items-center"
       >
         {" "}
-        <p className=" font-semibold pb-2 text-lg">Organisation Details</p>
+        <div>
+          <p className=" font-semibold pb-2 text-lg">Organisation Details</p>
+          <div className="grid grid-cols-2 gap-4  p-4 rounded-sm shadow-sm">
+            <Form.Item label=" Name" name="name" rules={[{ required: true }]}>
+              <Input placeholder="enter name" />
+            </Form.Item>
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="enter address" />
+            </Form.Item>
+            <Form.Item
+              label="Phone Number"
+              name="phoneNumber"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="enter phone number" />
+            </Form.Item>
+            <Form.Item
+              label="Branch ID"
+              name="branchId"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="enter max loan amount" />
+            </Form.Item>
+          </div>
+        </div>
+        <Form.Item className="mt-6">
+          <Button className=" w-full" type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
       </Form>
     </div>
   );
