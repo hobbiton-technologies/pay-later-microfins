e73f031976921ccada82b1e86d49472c1a342f53
SamWaku <samuelwaku1st@gmail.com>
2025-07-03 17:54:39 +0200

    chore: Organisation Request

---
+++ src/api/mutations/organisationMutation.ts
@@ -0,0 +1,20 @@
+import { Api } from "../apiSlice";
+
+export interface OrganisationData {
+  name: "string";
+  contactNo: "string";
+}
+
+const OrganisationRequest = Api.injectEndpoints({
+  endpoints: (builder) => ({
+    CreateOrganisation: builder.mutation<
+      any,
+      { organizationId: number; organisationData: OrganisationData }
+    >({
+      query: () => ({
+        url: ``,
+        method: "POST",
+      }),
+    }),
+  }),
+});
