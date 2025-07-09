5c0cd842b0518c1838f3a462d9564d62bda94a28
SamWaku <samuelwaku1st@gmail.com>
2025-07-09 12:12:31 +0200

    chore: staff table

--- src/modules/business/components/StaffTable.tsx
+++ src/modules/business/components/StaffTable.tsx
@@ -1,3 +1,155 @@
+import { StaffMembersData } from "@/api/queries/summaryQueries";
+import { Button, Dropdown, MenuProps, Space } from "antd";
+import Table, { ColumnsType } from "antd/es/table";
+import { ExportOutlined, EyeOutlined } from "@ant-design/icons";
+import DebouncedInputField from "@/modules/components/DebouncedInput";
+import { useState } from "react";
+
+export const branchesColumns: ColumnsType<StaffMembersData> = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Full Name",
+    dataIndex: "user",
+    render: (_, record: StaffMembersData) =>
+      `${record.user.firstName + "" + record.user.lastName} `,
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+
+  {
+    title: "Phone Number",
+    dataIndex: "phoneNumber",
+    key: "phoneNumber",
+  },
+  {
+    title: "Branch ID",
+    dataIndex: "branchId",
+    key: "branchId",
+  },
+  {
+    title: "Actions",
+    key: "actions",
+    render: () => {
+      const items: MenuProps["items"] = [
+        {
+          key: "4",
+          label: (
+            <span className="flex gap-2" onClick={() => alert("View CLicked")}>
+              <EyeOutlined />
+              View
+            </span>
+          ),
+        },
+      ];
+
+      return (
+        <Space>
+          <Dropdown menu={{ items }} placement="bottomRight">
+            <Button className=" dark:text-white">
+              <EyeOutlined />
+            </Button>
+          </Dropdown>
+        </Space>
+      );
+    },
+  },
+];
+
 export const StaffTable = () => {
-  return <div>StaffTable</div>;
+  const [id, setSearchId] = useState<string>("");
+
+  const [isCreateDrawerVisible, setIsCreateDrawerVisible] = useState(false);
+  const [pageNumber, setPageNumber] = useState<number | null>(1);
+  const [pageSize, setPageSize] = useState(10);
+
+  const handleTableChange = (pagination: any) => {
+    setPageNumber(pagination.current);
+    setPageSize(pagination.pageSize);
+  };
+
+  const handleSearch = () => {
+    setSearchId(id);
+  };
+
+  const handleSearchClear = () => {
+    setSearchId(id);
+  };
+
+  return (
+    <div className=" mt-2">
+      {" "}
+      <section className="w-full h-full py-3 flex   gap-2 ">
+        <div className="w-full">
+          <DebouncedInputField
+            placeholder="Search for Staff Member"
+            onSearch={handleSearch}
+            onClear={handleSearchClear}
+            allowClear={true}
+          />
+        </div>
+        <div className=" flex gap-3">
+          {/* <Button onClick={exportCSV} className="bg-green-700 text-gray-100">
+                    <ExportOutlined className="text-white" />
+                    Export to CSV
+                  </Button> */}
+          {/* <Select placeholder="Status" className="min-w-32" allowClear={true}>
+                    <Select.Option value={105}>Pending</Select.Option>
+                    <Select.Option value={109}>Failed</Select.Option>
+                    <Select.Option value={100}>Successful</Select.Option>
+                  </Select> */}
+          <Button
+            type="primary"
+            onClick={() => setIsCreateDrawerVisible(true)}
+            className=""
+          >
+            <ExportOutlined className="" />
+            Add Staff Member
+          </Button>
+        </div>
+      </section>
+      <section className="w-full h-full hidden md:flex md:flex-col">
+        <Table
+          // dataSource={microfinBranches?.data || []}
+          columns={branchesColumns}
+          rowKey="id"
+          onChange={handleTableChange}
+          // loading={{
+          //   spinning: isFetching,
+          //   indicator: customLoader,
+          // }}
+          // pagination={{
+          //   current: pageNumber ?? 1,
+          //   pageSize: pageSize,
+          //   total: microfinBranches?.totalItems,
+          // }}
+          components={{
+            header: {
+              cell: (props: any) => (
+                <th
+                  {...props}
+                  className="border-b-2 !bg-white !text-gray-400 text-xs !font-normal "
+                >
+                  {props.children}
+                </th>
+              ),
+            },
+            body: {
+              cell: (props: any) => (
+                <td {...props} className=" border-gray-300  text-xs  ">
+                  {props.children}
+                </td>
+              ),
+            },
+          }}
+        />
+      </section>
+    </div>
+  );
 };
