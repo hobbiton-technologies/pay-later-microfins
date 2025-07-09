ef17d0e867c2dabf6eb07486b452f06e912fcd91
SamWaku <samuelwaku1st@gmail.com>
2025-07-09 17:58:26 +0200

    chore: microfin

--- src/modules/business/components/MIcrofinOrgStaffMemberForm.tsx
+++ src/modules/business/components/MIcrofinOrgStaffMemberForm.tsx
@@ -1,19 +1,18 @@
 import { Form } from "antd";
-import { number } from "framer-motion";
 
-type UserItemProps = {
-  name: number;
-  fieldKey: React.Key;
-};
+// type UserItemProps = {
+//   name: number;
+//   fieldKey: React.Key;
+// };
 
-const UserItem: React.FC<UserItemProps> = ({ name, fieldKey }) => {
-  return (
-    <div
-      key={fieldKey}
-      className="grid grid-cols-1 gap-4 mb-4 shadow-sm p-4 rounded relative bg-white"
-    ></div>
-  );
-};
+// const UserItem: React.FC<UserItemProps> = ({ name, fieldKey }) => {
+//   return (
+//     <div
+//       key={fieldKey}
+//       className="grid grid-cols-1 gap-4 mb-4 shadow-sm p-4 rounded relative bg-white"
+//     ></div>
+//   );
+// };
 
 export const MicrofinOrgStaffMemberForm = () => {
   const [form] = Form.useForm();
