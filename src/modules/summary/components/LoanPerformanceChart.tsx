80c30dceb036aa683a3b418115d3a51bfcee8d66
SamWaku <samuelwaku1st@gmail.com>
2025-07-14 09:07:03 +0200

    chore: setting up chart

--- src/modules/summary/components/LoanPerformanceChart.tsx
+++ src/modules/summary/components/LoanPerformanceChart.tsx
@@ -1,3 +1,21 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export const LoanPerformaceChart = () => {
-  return <div className=" shadow-sm rounded-md p-4">Loan Chart</div>;
+  return (
+    <div className=" shadow-sm rounded-md p-4">
+      <Card className="h-full flex flex-col">
+        <CardHeader>
+          <CardTitle>Loan Perfomance by month</CardTitle>
+          {/* <CardDescription>January - December 2025</CardDescription> */}
+        </CardHeader>
+      </Card>
+    </div>
+  );
 };
